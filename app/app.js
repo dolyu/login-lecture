34beb8a3e432d757675dbfab1c9b5149671d16c3
dolyu <dolyu@nate.com>
2021-02-26 14:29:32 +0900

    폴더 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const express = require("express");
+const app = express();
+
+
+//앱세팅
+app.set("views", "src/views");
+app.set("view engine", "ejs");
+
+const home = require("./src/routes/home"); // index.js읽는다
+app.use("/",home); // use -> 미들웨어 등록
+
+module.exports = app;
+
+
