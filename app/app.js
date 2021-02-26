7bdc5635dd7b91d4d9e810b6becf1fbb1258dcbf
dolyu <dolyu@nate.com>
2021-02-26 15:21:14 +0900

    nodemon

--- app/app.js
+++ app/app.js
@@ -5,8 +5,9 @@ const app = express();
 
 
 //앱세팅
-app.set("views", "src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 const home = require("./src/routes/home"); // index.js읽는다
 app.use("/",home); // use -> 미들웨어 등록
